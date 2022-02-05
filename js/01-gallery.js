79daa4e2cee79ab1c363fd2a3f85d9cda7c64988
namhels <8.lsv.888@gmail.com>
2022-02-05 14:28:05 +0200

    добавил библиотеки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,78 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
 
-console.log(galleryItems);
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+// Реализация делегирования на div.gallery и получение url большого изображения.
+// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// Замена значения атрибута src элемента < img > в модальном окне перед открытием.Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+
+//   Разметка элемента галереи
+// Ссылка на оригинальное изображение должна храниться в data - атрибуте source на элементе < img >, и указываться в href ссылки.Не добавляй другие HTML теги или CSS классы кроме тех, что есть в этом шаблоне.
+
+//   Закрытие с клавиатуры
+// ВНИМАНИЕ
+// Этот функционал не обязателен при сдаче задания, но будет хорошей дополнительной практикой.
+
+// Добавь закрытие модального окна по нажатию клавиши Escape.Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно.У библиотеки basicLightbox есть метод для программного закрытия модального окна.
+
+//   <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>
+
+import { galleryItems } from "./gallery-items.js";
+
+const galleryRef = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+    //   src="${preview}"
+    //   data-source="${original}"
+    //   alt="${description}"
+    // />
+  </a>
+</div>`;
+  })
+  .join("");
+
+galleryRef.insertAdjacentHTML("afterbegin", markup);
+
+let instance;
+
+function onModalOpen(event) {
+  event.preventDefault();
+
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
+  window.addEventListener("keydown", onEscModalClose);
+
+  const imageSrc = event.target.dataset.source;
+  instance = basicLightbox.create(
+    `
+		<img width="1400" height="900" src="${imageSrc}">
+	`,
+    {
+      onClose: () => window.removeEventListener("keydown", onEscModalClose),
+    }
+  );
+  instance.show();
+}
+
+function onEscModalClose(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
+
+galleryRef.addEventListener("click", onModalOpen);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Выполняй это задание в файлах 02-lightbox.html и 02-lightbox.js. Разбей его на несколько подзадач:
 
-console.log(galleryItems);
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. Используй готовый код из первого задания.
+// Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. Необходимо добавить ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+// Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery. Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+// Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt.Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+
+/* <a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a>; */
+
+import { galleryItems } from "./gallery-items.js";
+
+const galleryRef = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+                <img
+                  class="gallery__image"
+                  src="${preview}"
+                  alt="${description}"
+                />
+              </a>`;
+  })
+  .join("");
+
+galleryRef.insertAdjacentHTML("afterbegin", markup);
+
+var lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "outside",
+});
